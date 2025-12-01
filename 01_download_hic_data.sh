ac016e12ffb818ec78dd2c9b30d1826077d69ebb
LaurenHuet <88010555+LaurenHuet@users.noreply.github.com>
2025-12-01 15:05:57 +0800

    Create script for downloading HIC data
    Added a script to download HIC data based on run parameters.

--- 01_download_hic_data.sh
+++ 01_download_hic_data.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+RUN=$1
+RUNDIR=$2
+RUNID=$(bs list run | grep $RUN | awk '{print $4}')
+mkdir -p $RUNDIR/$RUN
+
+#this creates the list of all the lanes for downloading
+
+bs list dataset --input-run $RUNID | awk '{print $2;}' > $RUN.prefix.txt 
+sed -i '1,3d' $RUN.prefix.txt
+
+for PREFIX in $(cat $RUN.prefix.txt); do
+ID=$(bs list dataset --input-run $RUNID | grep $PREFIX | awk '{print $4;}')
+echo $PREFIX $ID ">>" $RUNDIR/$RUN/$PREFIX
+bs download dataset ---input-run $RUNID -i $ID -o $RUNDIR/$RUN/$PREFIX
+done
